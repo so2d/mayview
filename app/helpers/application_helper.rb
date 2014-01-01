9936e8affda6bc23adf419a99983ba0104a22c37
Sola Otudeko <so2d@icloud.com>
2014-01-01 15:02:44 +0000

    Adjust title

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -4,10 +4,11 @@ module ApplicationHelper
   # Returns the full title on a per-page basis.
   def full_title(page_title)
     base_title = "Mayview Guest House"
+    append_to_title = "Quality Bed and Breakfast in Southampton City Centre"
     if page_title.empty?
-      base_title
+      "base_title | append_to_title"
     else
-      "#{base_title} | #{page_title}"
+      "#{base_title} | #{page_title} - #{append_to_title}"
     end
   end
   
